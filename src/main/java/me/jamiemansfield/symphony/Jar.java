cb11827c7c6066addd4d342e761f445167515c8b
Jamie Mansfield <dev@jamierocks.uk>
2018-11-04 13:01:39 +0000

    jar: Deobfuscate classes before handing to ForgeFlower

--- src/main/java/me/jamiemansfield/symphony/Jar.java
+++ src/main/java/me/jamiemansfield/symphony/Jar.java
@@ -7,11 +7,20 @@
 
 package me.jamiemansfield.symphony;
 
+import org.cadixdev.bombe.analysis.CachingInheritanceProvider;
+import org.cadixdev.bombe.analysis.InheritanceProvider;
+import org.cadixdev.bombe.asm.analysis.ClassProviderInheritanceProvider;
 import org.cadixdev.bombe.asm.jar.ClassProvider;
 import org.cadixdev.bombe.jar.AbstractJarEntry;
 import org.cadixdev.bombe.jar.Jars;
 import org.cadixdev.bombe.util.ByteStreams;
 import org.cadixdev.lorenz.MappingSet;
+import org.cadixdev.lorenz.model.ClassMapping;
+import org.cadixdev.survey.remapper.SurveyRemapper;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.commons.ClassRemapper;
+import org.objectweb.asm.commons.Remapper;
 
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
@@ -32,6 +41,9 @@ public class Jar implements ClassProvider, Closeable {
 
     private final JarFile jar;
     private final MappingSet mappings = MappingSet.create();
+    private final InheritanceProvider inheritanceProvider =
+            new CachingInheritanceProvider(new ClassProviderInheritanceProvider(this));
+    private final Remapper remapper = new SurveyRemapper(this.mappings, this.inheritanceProvider);
 
     public Jar(final JarFile jar) {
         this.jar = jar;
@@ -53,8 +65,7 @@ public class Jar implements ClassProvider, Closeable {
         return Jars.walk(this.jar);
     }
 
-    @Override
-    public byte[] get(final String klass) {
+    private byte[] getObfuscated(final String klass) {
         final String internalName = klass + ".class";
 
         final JarEntry entry = this.jar.getJarEntry(internalName);
@@ -68,6 +79,25 @@ public class Jar implements ClassProvider, Closeable {
         catch (final IOException ignored) {
             return null;
         }
+    }
+
+    @Override
+    public byte[] get(final String klass) {
+        // deobf -> obf
+        final ClassMapping<?, ?> mapping = this.mappings.reverse().getOrCreateClassMapping(klass);
+
+        // Get obfuscated bytecode
+        final byte[] obfuscated = this.getObfuscated(mapping.getFullDeobfuscatedName());
+        if (obfuscated == null) return null;
+
+        // Remap the class
+        final ClassReader reader = new ClassReader(obfuscated);
+        final ClassWriter writer = new ClassWriter(reader, 0);
+        reader.accept(new ClassRemapper(
+                writer,
+                this.remapper
+        ), 0);
+        return writer.toByteArray();
     }
 
     @Override
